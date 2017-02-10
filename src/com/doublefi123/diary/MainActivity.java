098778b2529318ee99e2679b9fa344ba597ff17b
chason <7641436@QQ.com>
2017-02-10 15:16:30 +0800

    圆形头像

---
+++ src/com/doublefi123/diary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.doublefi123.diary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.doublefi123.diary.widget.CircularImage;
+/**
+ * 
+ * @author doublefi123
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		CircularImage cover_user_photo = (CircularImage) findViewById(R.id.cover_user_photo);
+		cover_user_photo.setImageResource(R.drawable.face);
+	}
+}
